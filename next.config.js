2ad8cdb671097058c1381219c2654e590a621d5d
mollybeach <mollyjbeach@gmail.com>
2024-11-25 13:28:38 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  ...(process.env.NEXT_PUBLIC_NODE_ENV === 'production' ? { output: 'export' } : {}),
+ // ...(process.env.NEXT_PUBLIC_NODE_ENV === 'production' ? { output: 'export' } : {}),
+ output: 'export',
   images: {
     unoptimized: true,
     domains: ['res.cloudinary.com'],
