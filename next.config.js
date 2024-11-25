7efecbf927ff6e5ac935310bb02153205013bf8c
mollybeach <mollyjbeach@gmail.com>
2024-11-25 13:32:13 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
     unoptimized: true,
     domains: ['res.cloudinary.com'],
   },
-  basePath: process.env.NEXT_PUBLIC_NODE_ENV === 'production' ? '/megaverse-polynet' : '',
-  assetPrefix: process.env.NEXT_PUBLIC_NODE_ENV === 'production' ? '/megaverse-polynet' : '',
+  basePath: process.env.NEXT_PUBLIC_NODE_ENV === 'production' ? '/megaverse' : '',
+  assetPrefix: process.env.NEXT_PUBLIC_NODE_ENV === 'production' ? '/megaverse' : '',
 };
 
 module.exports = nextConfig;
