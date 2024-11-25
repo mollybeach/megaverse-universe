ad61fbe991f505aed7287550d3e0df368fa4b4df
mollybeach <mollyjbeach@gmail.com>
2024-11-25 15:18:20 -0800

    Update paths.ts

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -2,7 +2,6 @@ export const getApiPath = (endpoint: string) => {
     const environment = process.env.NEXT_PUBLIC_NODE_ENV || 'production';
     const basePath = environment === 'production' ? '/megaverse-universe' : '';
     
-    // Log the configuration
     console.log('API Configuration:', {
       environment,
       basePath,
