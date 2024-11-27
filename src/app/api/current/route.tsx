e36721068e04c8337437f73e9359bac0a5071017
Molly Beach <mollyjbeach@gmail.com>
2024-11-26 20:41:11 -0800

    Update route.tsx

--- src/app/api/current/route.tsx
+++ src/app/api/current/route.tsx
@@ -5,10 +5,13 @@
 */
 import { NextResponse, NextRequest } from 'next/server';
 import {  setPhase } from '@/lib/state/phaseState';
-//export const runtime = 'nodejs';
-//export const dynamic = 'force-dynamic';
-export const runtime = 'edge';
+//export const runtime = 'nodejs';export const dynamic = 'force-dynamic';
+
+//export const runtime = 'edge';
 export const revalidate = 60;
+export const forceDynamic = 'force-dynamic';
+export const runtime = 'nodejs' || 'edge';
+
 interface Payload {
     candidateId: string | undefined;
     row: number;
